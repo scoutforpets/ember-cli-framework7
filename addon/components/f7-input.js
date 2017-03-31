30e3987442232412eee4982c7e2d829ea6af8867
James Dixon <jim.w.dixon@gmail.com>
2017-03-30 19:26:20 -0600

    use ember one-way-controls for input

--- addon/components/f7-input.js
+++ addon/components/f7-input.js
@@ -3,5 +3,16 @@ import layout from '../templates/components/f7-input';
 
 export default Ember.Component.extend({
   layout,
-  classNames: ['item-content']
+  classNames: ['item-content'],
+  classNameBindings: ['value:not-empty-state'],
+
+  type: 'text',
+
+  isTextArea: Ember.computed('type', function() {
+    if (this.get('type') === 'textarea') {
+      return true;
+    }
+
+    return false;
+  })
 });
